3b502c7c7acab4853081ac944fcd0cc1ea492991
Danish Mullaji <123233395+felixalpine@users.noreply.github.com>
2023-02-25 13:38:11 +0000

    Feb 25, 2023, 1:38 PM

--- weird.py
+++ weird.py
@@ -2,8 +2,10 @@ n = int(input())
 
 while True:
     if 1 <= n <= 100 and n % 2 == 0:
-        if 2 <= n <= 5:
-            print("even")
+        if 2 <= n <= 5 or n > 20:
+            print("not weird")
+            if 6 <= n <=20:
+                print("weird")
         else:
             print("Weird")
     else:
